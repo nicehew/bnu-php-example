571a4837bd719b62b40a1817728066de69578487
nicehew <hew_is_better_than_hans@hotmail.com>
2019-05-24 00:27:43 +0100

    finfr

--- templates/login.php
+++ templates/login.php
@@ -4,25 +4,51 @@
 <form name="frmLogin" action="authenticate.php" method="post">
   <table align="center" class="table2">
 
+</br>
+
+<tr>
+  <td>
+  </td>
+</tr>
+
     <tr>
-      <td>Student ID:</td>
+      <td>Student ID</td>
+    </tr>
+    <tr>
       <td>
    <input name="txtid" type="text" />
  </td>
 </tr>
 
+<tr>
+  <td>
+  </td>
+</tr>
+
    <tr>
-     <td>Password:</td>
+     <td>Password</td>
+   </tr>
+   <tr>
      <td>
    <input name="txtpwd" type="password" />
  </td>
 </tr>
 
+<tr>
+  <td>
+  </td>
+</tr>
+
    <tr>
-     <td></td>
      <td>
    <input type="submit" value="Login" name="btnlogin" />
  </td>
  </tr>
+
+ <tr>
+   <td>
+   </td>
+ </tr>
+ 
 </table>
 </form>
